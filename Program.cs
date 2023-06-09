bfc001deb7e6975bacfee5270351ecc3bb297c8a
azizlibayim <bayimsha@code.edu.az>
2023-06-10 02:42:41 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DATE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a date in the format dd.MM.yyyy:");
+                string userInput = Console.ReadLine();
+
+                DateTime date = DateTime.ParseExact(userInput, "dd.MM.yyyy", null);
+                Console.WriteLine("Converted date: " + date);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+    }
+}
